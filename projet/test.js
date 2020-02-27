64a87320d706a822512fe991475db3efc76da02c
80010-09-03 <80010-09-03@exchange.ad.afpanet>
2020-02-27 16:53:02 +0100

    // Type de variable
    // var myVar; // le type de la variable est undefined
    // myVar = 324; // type number
    // myVar = "Bonjour"; // type string
    // myVar = true; // type boolean
    // myVar = new Array(); // type object
    
            // Affichage des valeurs dans la console
    
    // console.log(typeof 42); // Affiche "number"
    // console.log(typeof 'blubber'); // Affiche "string"
    // console.log(typeof true); // Affiche "boolean"
    // var myVar;
    // console.log(typeof myVar); // Affiche 'undefined'
    
            // Boîte de dialogue : méthode alert()
    
    // var num = 1 ;
    // var prenom = "Dave" ;
    // alert(num);
    // window.alert(nom + " " + "Dave");
    
            // Méthode write()
    
    // var age = 35
    // document.write("<b>Votre âge est :</b> " + age + "ans");
    
            // Méthode prompt()
    
    // var reponse1 = window.prompt("Saisissez votre nom");
    // var reponse2 = window.prompt("Saisissez votre prénom");
    
            // Méthode confirm()
    
    // if (window.confirm("Voulez-vous continuer ?") == true) {}
    
            /*// Exercice 1
     var hi ="Bonjour";
     var welcome = "Bienvenue sur notre site internet";
     var reponse1 = window.prompt("Entrez votre nom");
     var reponse2 = window.prompt("Entrez votre prénom");
     if (window.confirm("Êtes-vous un homme") == true);
     {
      // window.alert(hi + " " + welcome + " " + reponse1 + " " + reponse2)
     }
    
            Exercice 2
     var a = "100";
     var b = 100;
     var c = 1.00;
     var d = true;
     var e = false
     window.alert("Ceci est une chaîne de caractères :" + " " + a);
     window.alert(b--);
     window.alert(c=a);
     window.alert(d=e);
    
            Exemple 1
    
    var temp = window.prompt("Saisissez la température");
    if (temp > 38);
    {
        window.alert("Le patient a de la fièvre");
    }
    
            Exemple 2
    
    var temp = window.prompt("Saisissez la température");
    var tens = window.prompt("Saisissez la tension");
    if (temp > 41 && tens > 25)
    {
        window.alert("Le patient va perdre patience");
    }
            Exemple 3
    
    if (window.confirm("Êtes-vous patient ?") == false)
    {
        window.alert("Econduire l'olibrius");
    }
    
            Exemple 4
    
    var temp = window.prompt("Saisissez la température");
    var tens = window.prompt("Saisissez la tension");
    var pouls = window.prompt("Saisissez le pouls");
    if (temp > 42 || (tens < 25 && pouls > 180))
    {
        window.alert("Prévenir la famille")
    }
    
            Exemple 5
    
    var temp = window.prompt("Saisissez la température");
    var tens = window.prompt("Saisissez le pouls");
    if (temp > 40 || tens >= 25)
    {
        window.alert("Hospitaliser le patient");
    }
    
            Exemple 6
    
    var pat = window.confirm("Patient vivant") == true;
    var pouls = window.prompt("Saisissez le pouls");
    if (pat == false && pouls == 0)
    {
        window.alert("Appeler le curé");
    }
    
            //*/

---
+++ projet/test.js
@@ -0,0 +1,111 @@
+        // Type de variable
+
+// var myVar; // le type de la variable est undefined
+// myVar = 324; // type number
+// myVar = "Bonjour"; // type string
+// myVar = true; // type boolean
+// myVar = new Array(); // type object
+
+        // Affichage des valeurs dans la console
+
+// console.log(typeof 42); // Affiche "number"
+// console.log(typeof 'blubber'); // Affiche "string"
+// console.log(typeof true); // Affiche "boolean"
+// var myVar;
+// console.log(typeof myVar); // Affiche 'undefined'
+
+        // Boîte de dialogue : méthode alert()
+
+// var num = 1 ;
+// var prenom = "Dave" ;
+// alert(num);
+// window.alert(nom + " " + "Dave"); 
+
+        // Méthode write()
+
+// var age = 35
+// document.write("<b>Votre âge est :</b> " + age + "ans");
+
+        // Méthode prompt()
+
+// var reponse1 = window.prompt("Saisissez votre nom");
+// var reponse2 = window.prompt("Saisissez votre prénom");
+
+        // Méthode confirm()
+
+// if (window.confirm("Voulez-vous continuer ?") == true) {}
+
+        /*// Exercice 1
+ var hi ="Bonjour";
+ var welcome = "Bienvenue sur notre site internet";
+ var reponse1 = window.prompt("Entrez votre nom");
+ var reponse2 = window.prompt("Entrez votre prénom");
+ if (window.confirm("Êtes-vous un homme") == true);
+ {
+  // window.alert(hi + " " + welcome + " " + reponse1 + " " + reponse2)
+ }
+
+        Exercice 2
+ var a = "100";
+ var b = 100;
+ var c = 1.00;
+ var d = true;
+ var e = false
+ window.alert("Ceci est une chaîne de caractères :" + " " + a);
+ window.alert(b--);
+ window.alert(c=a);
+ window.alert(d=e); 
+
+        Exemple 1
+
+var temp = window.prompt("Saisissez la température");
+if (temp > 38);
+{
+    window.alert("Le patient a de la fièvre");
+}
+
+        Exemple 2
+
+var temp = window.prompt("Saisissez la température");
+var tens = window.prompt("Saisissez la tension");
+if (temp > 41 && tens > 25)
+{
+    window.alert("Le patient va perdre patience");
+}
+        Exemple 3
+
+if (window.confirm("Êtes-vous patient ?") == false)
+{
+    window.alert("Econduire l'olibrius");
+}
+
+        Exemple 4
+
+var temp = window.prompt("Saisissez la température");
+var tens = window.prompt("Saisissez la tension");
+var pouls = window.prompt("Saisissez le pouls");
+if (temp > 42 || (tens < 25 && pouls > 180))
+{
+    window.alert("Prévenir la famille")
+}
+
+        Exemple 5
+
+var temp = window.prompt("Saisissez la température");
+var tens = window.prompt("Saisissez le pouls");
+if (temp > 40 || tens >= 25)
+{
+    window.alert("Hospitaliser le patient");
+}
+
+        Exemple 6
+
+var pat = window.confirm("Patient vivant") == true;
+var pouls = window.prompt("Saisissez le pouls");
+if (pat == false && pouls == 0)
+{
+    window.alert("Appeler le curé");
+}
+
+        //*/
+
